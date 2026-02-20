416752bfcdaeb222c6fbf987026b65334f22d535
Sami Kojo <sami.kojo@tuni.fi>
2026-02-20 16:43:24 +0200

    Dash

--- code/character.gd
+++ code/character.gd
@@ -4,6 +4,8 @@ extends CharacterBody2D
 @export var _speed : float = 300.0
 @export var _jump_velocity : float = -400.0
 
+var _dash_timer : float = 0
+
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -13,13 +15,21 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = _jump_velocity
+	
+	if Input.is_action_just_pressed("dash"):
+		_dash_timer = 0.25
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * _speed
+	if _dash_timer > 0:
+		_dash_timer = clamp(_dash_timer - delta, 0, _dash_timer)
+		velocity.x = _speed * 3
+	
 	else:
-		velocity.x = move_toward(velocity.x, 0, _speed)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_axis("ui_left", "ui_right")
+		if direction:
+			velocity.x = direction * _speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, _speed)
 
 	move_and_slide()
